3b197638b0c03b7105eab9dceafff7068d2afc1c
Kushagra <kushagragoyalkkg@gmail.com>
2022-10-31 07:05:36 +0530

    version-1

---
+++ source/script.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", bindEvents);
+
+function bindEvents() {
+
+    let today = new Date();
+    let hr = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+    let time = setTimeout(function () { bindEvents() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
